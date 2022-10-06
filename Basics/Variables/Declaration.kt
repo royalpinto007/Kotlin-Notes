140f5997cbebcec66c595a35fe5c3c6d257989e7
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-06 09:00:42 +0530

    Create Declaration.kt

---
+++ Basics/Variables/Declaration.kt
@@ -0,0 +1,15 @@
+Syntax-
+val name : datatype = value
+or
+val name = value
+
+Example-
+val count: Int = 2
+val count = 2
+
+Use of $ to use values in a string-
+fun main() {
+    val count: Int = 2
+    println("You have $count unread messages.")
+}
+Output- You have 2 unread messages.
